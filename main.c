8a997b4f311f6763686be3da42564ed57b55e1d5
winterle <leanderwinter@hotmail.de>
2018-05-18 19:57:23 +0200

    updated parsing

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <memory.h>
 #include <time.h>
 
@@ -7,12 +8,15 @@
 #define UNKNOWN_CHAR 2
 #define UINT_OVERFLOW 3
 #define TOO_MANY_NUMBERS 4
+#define NOT_ENOUGH_NUMBERS 5
 /* global variables and structs */
 
 /* function declarations */
 void die(unsigned short error_num);
-unsigned short getNextLine();
+unsigned short getNextLine(char *num0,char *num1);
 void parseInput();
+
+
 int main() {
     parseInput();
     return 0;
@@ -20,27 +24,31 @@ int main() {
 
 void parseInput(){
     unsigned short rc;
-    while((rc = getNextLine())!=1) {
+    char num0[11];
+    char num1[11];
+    while((rc = getNextLine(num0,num1))!=1) {
         if (rc != 0) {
             die(rc);
         };
+        printf("num0 = %s , num1 = %s\n",num0,num1);
     }
 }
 
 /**
  * reads the next line (until '\ n') as ASCII-encoded byte-stream and handles errors
+ * @param 2 Arrays of length 11 to save the parsed numbers in. Any other length results in undefined behavior!
  * @returns 0 on success, 1 when reaching EOF, defined error codes when encountering errors
  * */
-unsigned short getNextLine(){
+unsigned short getNextLine(char *num0,char *num1){
     char c;
-    char num0[11];
-    char num1[11];
     num0[10] = num1[10] = '\0';
+    num0[0] = num1[0] = '\0'; //to check if the second number was actually read
     short numbersRead = 0;
     short lastRead = 0; //boolean: lastRead char was /n or blank: 0, else 1
     char *pos = num0;
     while((c=fgetc(stdin))!='\n' && c != EOF){
         if(c == ' ' && lastRead==1){
+            lastRead=0;
             pos = num1;
             numbersRead++;
         }
@@ -70,10 +78,11 @@ unsigned short getNextLine(){
         else if(numbersRead > 1)
             return TOO_MANY_NUMBERS;
     }
-    printf("%s\n",num0);
-    printf("%s\n",num1);
     if(c == EOF){
         return 1;
+    }
+    if(num1[0] == '\0' || num0[0] == '\0'){
+        return NOT_ENOUGH_NUMBERS;
     }
     return 0;
 }
@@ -87,6 +96,9 @@ void die(unsigned short error_num){
     }
     else if(error_num==TOO_MANY_NUMBERS){
         fprintf(stderr,"Too many Numbers in one Line, exiting...\n");
+    }
+    else if(error_num==NOT_ENOUGH_NUMBERS){
+        fprintf(stderr,"Not enough Numbers in one Line, exiting...\n");
     }
     else{
         fprintf(stderr, "An unknown error occured, exiting...\n");
